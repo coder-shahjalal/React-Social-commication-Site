e4302a6d115af026348766d639d9cee85e46349c
coder-shahjalal <shahjalal.freelancer@gmail.com>
2020-09-06 13:22:30 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+import Home from './components/Home/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import NoMatch from './components/NoMatch/NoMatch';
+import PostDetail from './components/PostDetail/PostDetail';
+
+function App() {
+  return (
+    <div>
+      <Router>
+           <Switch>
+          <Route path="/home">
+             <Home/>
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          
+          <Route exact path="/post/:id">
+            <PostDetail />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
